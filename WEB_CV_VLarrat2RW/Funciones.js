77af959828eba4bfacba2bc2407db4b8647e41e1
$ git config --global user.name "VLarrat <$ git config --global user.email vlarratedith@gmail.com>
2023-02-22 12:20:22 +0100

    Ajustes finales

--- WEB_CV_VLarrat2RW/Funciones.js
+++ WEB_CV_VLarrat2RW/Funciones.js
@@ -11,43 +11,22 @@ btnMenu.addEventListener("click", function(){
     menu.classList.toggle("mostrar");
 });
 
+const submenubtn = document.querySelectorAll(".submenu-btn");
+for(let i=0; i<submenubtn.length;i++){
+    submenubtn[i].addEventListener("click",function(){
+        if(window.innerWidth <1024){
+        const submenu = this.nextElementSibling;
+        const height = submenu.scrollHeight;
+        
+        if(submenu.classList.contains("desplegar")){
+            submenu.classList.remove("desplegar");
+            submenu.removeAttribute("style");
+        } else{
+            submenu.classList.add("desplegar");
+            submenu.style.height = height + "px";
+        }
+     }  
+    });
+}
 
 
-
-
-//button.addEventListener("click", window.print());
-
-
-//button.addEventListener("click", window.print());
-
-
-
-
-
-//document.getElementById ('Boton').onmouseover = +window.print ();
-
-
-
-
-
-
-
-
-//function imprimir() {
-//	window.print();
-//}
-   
-
-
-//function imprimir() //
-
-
-//
-//{
- //   ("Lo logre por fin");
-
-
-//document.getElementById("Demo").innerHTML = "Por fin salio esto carajo";
-//}
-// 
-//<button onclick="window.print()">Imprimir pantalla con otro ejemplo</button>
